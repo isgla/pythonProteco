9c670a74220f352da8337c4f7c3ee6994456f888
isgla <hannahglz@ciencias.unam.mx>
2019-06-10 10:36:16 -0500

    Introduccion y presentacion

---
+++ Clase1/holamundo.py
@@ -0,0 +1,61 @@
+#Hola mundo
+print("Hola mundo")
+
+#Operaciones
+
+print(5+7)
+print((4+1j) + (2+3j))
+ 
+#Division
+print(7/2)
+
+#Division entera
+print(7//2)
+
+#Modulo
+print(10%8)
+
+x=5
+y=2.5
+z=x+y
+print(z)
+print(type(z))
+
+#Potencias
+print(5**2)
+
+#Booleanos
+print(x>y)
+print(x!=y)
+
+a = True
+b = False
+# and, or, not
+
+print(a and b)
+print(a or b)
+print(not a)
+print((a and b) and (a or b))
+print((a and b) or (a or b))
+print(type(a))
+
+
+"""
+Tipos de datos:
+º Numericos
+	-Enteros
+	-Flotantes
+	-Complejos
+
+º Booleanos
+	-True
+	-False
+	Operadores relaciones
+	>
+	<
+	>=
+	<=
+	==
+	!=
+
+"""
